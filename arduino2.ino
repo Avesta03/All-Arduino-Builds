b9ce267dcd3fbc93b418bdd64f48804ddd046f1d
Avesta03 <avestaafsharimehr@gmail.com>
2023-05-01 17:26:08 +0100

    Started Functions

--- arduino2.ino
+++ arduino2.ino
@@ -86,3 +86,29 @@ while (i < 20)
     i ++; 
 }
 
+/// Functions ///
+
+const int ledPin = 10;
+const int delayPeriod = 250;
+
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+    for (int i = 0; i < 20; i ++)
+    {
+        flash();
+    }
+    delay(3000);
+}
+
+void flash() // Calling a function 'flash'
+{
+    digitalWrite(ledPin, HIGH);
+    delay(delayPeriod);
+    digitalWrite(ledPin, LOW);
+    delay(delayPeriod);
+}
