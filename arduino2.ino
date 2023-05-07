c0a1f4389cab8eeffcb505d3962d10e7153d45db
Avesta03 <avestaafsharimehr@gmail.com>
2023-05-07 22:22:09 +0100

    Global, Local, & Static Variables

--- arduino2.ino
+++ arduino2.ino
@@ -145,3 +145,50 @@ void flash(int numFlashes, int d)
 // We have to declare the type of variable in the parameters (here they are both int) and are in fact defining new variables.
 // But these variables (numFlashes and d) can only be used within the flash function.
 // Only info needed from outside the function is to which pin the LED is attached.
+
+// Global, Local, and Static Variables 
+
+int x = 15;
+void indicate(int x)
+{
+    flash(x, 10);
+}
+
+// The above does not result in a compilation error but has the issue of two variables called x which can each have different values. 
+// The x on the first line is called a global variable (as it can be used anywhere in the program including inside any functions)
+// The local x inside the variable, however, has priority and so you cannot use the global variable x initially.
+// You can also define variables that aren't parameters but are just for use within a function, and these are called LOCAL variables. e.g:
+
+void indicate(int x)
+{
+    int timesToFlash = x * 2;
+    flash(timesToFlash, 10);
+}
+
+// The local variables (timesToFlash) will only exist while the function is running, and disappears when the function has finished its last command.
+
+// Static variables also exist and initialise the variable only the first time the function is run.
+
+const int ledPin = 10;
+const int delayPeriod = 250;
+
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+    static int count = 0;
+    digitalWrite(ledPin, HIGH);
+    delay(delayPeriod);
+    digitalWrite(ledPin, LOW);
+    delay(delayPeriod);
+    count ++;
+    if (count == 20)
+    {
+        count = 0;
+        delay(3000)
+    }
+}
+
