d228c3ba7fbfada76aa829a7f2536f87ed931527
Avesta03 <avestaafsharimehr@gmail.com>
2023-05-07 22:59:13 +0100

    Basic Morse Code SOS loop using 1 LED

---
+++ LEDmorsecodeSOSbasic.ino
@@ -0,0 +1,26 @@
+const int ledPin = 11;
+
+int durations[] = {200, 200, 200, 500, 500, 500, 200, 200, 200};
+
+void setup()
+{
+    pinMode(ledPin, OUTPUT);
+}
+
+void loop()
+{
+    for (int i = 0; i < 9; i++)
+    {
+        flash(durations[i]);
+    }
+    delay(1000);
+}
+
+void flash(int delayPeriod)
+{
+    digitalWrite(ledPin, HIGH);
+    delay(delayPeriod);
+    digitalWrite(ledPin, LOW);
+    delay(delayPeriod);
+}
+
