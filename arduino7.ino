67a64afc8a588033ef8f0914e3b87b77ac70ed3c
Avesta03 <avestaafsharimehr@gmail.com>
2023-07-11 15:26:38 +0100

    Implemented Code for Web Controlled Arduino. Now to apply.

--- arduino7.ino
+++ arduino7.ino
@@ -1,5 +1,5 @@
 //// IoT Programming
-// Need specialised Arduinos or shields to provide network capabilities for local network and internet - don't have this but will make notes.
+// Need specialised Arduinos or shields to provide network capabilities for local network and internet - we now have this and will make notes.
 
 /// HTTP (Hypertext Transport Protocol) is how Web browsers communicate with a Web server.
 // When you go to a page using a browser, it sends a request to the server host with a request, and the server processes it as soon as soon as it's received. 
@@ -61,3 +61,152 @@ void loop() // Responsible for servicing any requests that come to the Webs erve
 // Such a form has a selection of On and Off for each pin and an Update button that will send the pin settings to the Arduino.
 // When the Update button is pressed, a second req is sent to the Arduino, which will be just like the first req but will this time contain req parameters that contain the values of the pins!
 
+// NOTE: NEED TO IMPLEMENT THIS WITH ACQUIRED ESP8266 INTO ARDUINO BOARD.
+
+#include <SPI.h>
+#include <Ethernet.h>
+
+// MAC address just has to be unique. This should work
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED }; // EDIT TO FIT ESP8266?
+
+EthernetServer server(80);
+
+int numPins = 5;
+int pins[] = {3, 4, 5, 6, 7};
+int pinState[] = {0, 0, 0, 0, 0};
+char line1[100];
+
+void setup()
+{
+  for (int i = 0; i < numPins; i++)
+  {
+     pinMode(pins[i], OUTPUT);
+  }
+  Serial.begin(9600);
+  Ethernet.begin(mac);
+  server.begin();
+  Serial.print("Server started on: ");
+  Serial.println(Ethernet.localIP());
+}
+
+void loop()
+{
+  EthernetClient client = server.available();
+  if (client) 
+  {
+    while (client.connected()) 
+    {
+      readHeader(client);
+      if (! pageNameIs("/"))
+      {
+        client.stop();  
+        return;
+      }
+      client.println("HTTP/1.1 200 OK");
+      client.println("Content-Type: text/html");
+      client.println();
+
+      // send the body
+      client.println("<html><body>");
+      client.println("<h1>Output Pins</h1>");
+      client.println("<form method='GET'>");  
+      setValuesFromParams();
+      setPinStates();
+      for (int i = 0; i < numPins; i++)
+      {
+         writeHTMLforPin(client, i);
+      }
+      client.println("<input type='submit' value='Update'/>");
+      client.println("</form>");
+      client.println("</body></html>");
+
+      client.stop();            
+    }
+  }
+}
+
+void writeHTMLforPin(EthernetClient client, int i)
+{
+  client.print("<p>D");
+  client.print(pins[i]);  
+  client.print(" <select name='");
+  client.print(i);
+  client.println("'>");
+  client.print("<option value='0'");
+  if (pinState[i] == 0)
+  {
+    client.print(" selected");
+  }
+  client.println(">Off</option>");
+  client.print("<option value='1'");
+  if (pinState[i] == 1)
+  {
+    client.print(" selected");
+  }
+  client.println(">On</option>");
+  client.println("</select></p>");  
+}
+
+void setPinStates()
+{
+  for (int i = 0; i < numPins; i++)
+  {
+     digitalWrite(pins[i], pinState[i]);
+  }
+}
+
+void setValuesFromParams()
+{
+  for (int i = 0; i < numPins; i++)
+  {
+    pinState[i] = valueOfParam(i + '0');
+  }
+}
+
+void readHeader(EthernetClient client)
+{
+  // read first line of header
+  char ch;
+  int i = 0;
+  while (ch != '\n')
+  {
+    if (client.available())
+    {
+      ch = client.read();
+      line1[i] = ch;
+      i ++;
+    }
+  }
+  line1[i] = '\0'; 
+  Serial.println(line1);
+}
+
+boolean pageNameIs(char* name)
+{
+   // page name starts at char pos 4
+   // ends with space
+   int i = 4;
+   char ch = line1[i];
+   while (ch != ' ' && ch != '\n' && ch != '?')
+   {
+     if (name[i-4] != line1[i])
+     {
+       return false;
+     }
+     i++;
+     ch = line1[i];
+   }
+   return true;
+}
+
+int valueOfParam(char param)
+{
+  for (int i = 0; i < strlen(line1); i++)
+  {
+    if (line1[i] == param && line1[i+1] == '=')
+    {
+      return (line1[i+2] - '0');
+    }
+  }
+  return 0;
+}
